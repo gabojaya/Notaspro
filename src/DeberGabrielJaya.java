0b0ab05a0a4d4ae13c53ca7bcafb02909059cf04
gabojaya <118075791+gabojaya@users.noreply.github.com>
2022-12-06 15:13:16 -0500

    AUTOR

--- src/DeberGabrielJaya.java
+++ src/DeberGabrielJaya.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
  * Hay un lobo, una caperucita y unas uvas que quieren cruzan de un lado del rio al otro extremo.
  * Las reglas son: -si esta solo el lobo con la caperucita  (marcha la caperucita) o si la caperucita esta solo con las uvas (marchan las uvas)
  * @author Gabriel
+ * @date : 06.dic.2022
  * @version 2.0
  */
 public class DeberGabrielJaya {
